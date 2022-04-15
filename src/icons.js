80efba487160f67ecead663302296e0d338a813e
Luciano <lucianoeberhardt95@outlook.com>
2022-04-15 19:31:52 -0300

    icons set

---
+++ src/icons.js
@@ -0,0 +1,41 @@
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
+import {
+  faHome,
+  faFileImage,
+  faCubes,
+  faChevronRight,
+  faBars,
+  faTimes,
+  faList,
+  faThLarge,
+  faSpinner,
+  faSync,
+  faBookOpen,
+  faExternalLink,
+  faTrash,
+  faUserCircle,
+  faSignOut
+} from '@fortawesome/free-solid-svg-icons'
+
+import { faBlogger } from '@fortawesome/free-brands-svg-icons'
+
+library.add(
+  faHome,
+  faBlogger,
+  faFileImage,
+  faCubes,
+  faChevronRight,
+  faBars,
+  faTimes,
+  faList,
+  faThLarge,
+  faSpinner,
+  faSync,
+  faBookOpen,
+  faExternalLink,
+  faTrash,
+  faUserCircle,
+  faSignOut
+)
+
+dom.watch()
